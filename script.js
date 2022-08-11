ec9be1fde2aa04e4b325c2fab543be18a84f112e
jonathanmir <jonathanmir8@gmail.com>
2022-08-11 16:27:09 -0300

    11/08 final

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const nomeAluno = prompt("Insira o nome do aluno.");
 console.log(`O nome possui ${nomeAluno.length} letras`);
 if (nomeAluno.length < 5) {
-  console.log(`Nome de Aluno inválido!`);}
+  alert(`Nome de Aluno inválido!`);}
+else {console.log(`Nome válido!`
 
+)}
 let mat1 = "Matemática";
 let mat2 = "Português";
 let mat3 = "Informática";
@@ -18,170 +20,163 @@ let mat10 = "Educação Física";
 
 let notaMat = parseFloat(prompt(`Insira a nota ${mat1} do aluno.`));
 if (notaMat > 10 || notaMat < 0) {
-  console.log(`A nota é inválida!`);
-} else {
-  alert(`A nota de ${mat1} é ${notaMat}`);
+  alert(`A nota é inválida!`);
 }
+else {
 console.log(`A nota de ${mat1} é ${notaMat}`);
+}
 
 let presMat = parseInt(prompt(`Insira a presença de ${mat1} do aluno.`));
 if (presMat > 10 || presMat < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat1} é ${presMat}`);
+  console.log(`A presença de ${mat1} é ${presMat}`);
 }
 
 //Portugues
 
 let notaPort = parseFloat(prompt(`Insira a nota ${mat2} do aluno.`));
 if (notaPort > 10 || notaPort < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat2} é ${notaPort}`);
+  console.log(`A nota de ${mat2} é ${notaPort}`);
 }
-console.log(`A nota de ${mat2} é ${notaPort}`);
+
 
 let presPort = parseInt(prompt(`Insira a presença de ${mat2} do aluno.`));
 if (presPort > 10 || presPort < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat2} é ${presPort}`);
+  console.log(`A presença de ${mat2} é ${presPort}`);
 }
 
 //informática
 
 let notaInf = parseFloat(prompt(`Insira a nota ${mat3} do aluno.`));
 if (notaInf > 10 || notaInf < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat3} é ${notaInf}`);
+ console.log(`A nota de ${mat3} é ${notaInf}`);
 }
-console.log(`A nota de ${mat3} é ${notaInf}`);
 
 let presInf = parseInt(prompt(`Insira a presença de ${mat3} do aluno.`));
 if (presInf > 10 || presInf < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat3} é ${presInf}`);
+  console.log(`A presença de ${mat3} é ${presInf}`);
 }
 
 //Geografia
 
 let notaGeo = parseFloat(prompt(`Insira a nota ${mat4} do aluno.`));
 if (notaGeo > 10 || notaGeo < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat4} é ${notaGeo}`);
+  console.log(`A nota de ${mat4} é ${notaGeo}`);
 }
-console.log(`A nota de ${mat4} é ${notaGeo}`);
+
 
 let presGeo = parseInt(prompt(`Insira a presença de ${mat4} do aluno.`));
 if (presGeo > 10 || presGeo < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat4} é ${presGeo}`);
+  console.log(`A presença de ${mat4} é ${presGeo}`);
 }
 
 //Inglês
 
 let notaIng = parseFloat(prompt(`Insira a nota ${mat5} do aluno.`));
 if (notaIng > 10 || notaIng < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat5} é ${notaIng}`);
+  console.log(`A nota de ${mat5} é ${notaIng}`);
 }
-console.log(`A nota de ${mat5} é ${notaIng}`);
+
 
 let presIng = parseInt(prompt(`Insira a presença de ${mat5} do aluno.`));
 if (presIng > 10 || presIng < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat5} é ${presIng}`);
+  console.log(`A presença de ${mat5} é ${presIng}`);
 }
 
 //História
 
 let notaHist = parseFloat(prompt(`Insira a nota ${mat6} do aluno.`));
 if (notaHist > 10 || notaHist < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat6} é ${notaHist}`);
+  console.log(`A nota de ${mat6} é ${notaHist}`);
 }
-console.log(`A nota de ${mat6} é ${notaHist}`);
+
 
 let presHist = parseInt(prompt(`Insira a presença de ${mat6} do aluno.`));
 if (presHist > 10 || presHist < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat6} é ${presHist}`);
+  console.log(`A presença de ${mat6} é ${presHist}`);
 }
 
 //Filosofia
 
 let notaFil = parseFloat(prompt(`Insira a nota ${mat7} do aluno.`));
 if (notaFil > 10 || notaFil < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat7} é ${notaFil}`);
+  console.log(`A nota de ${mat7} é ${notaFil}`);
 }
-console.log(`A nota de ${mat7} é ${notaFil}`);
-
 let presFil = parseInt(prompt(`Insira a presença de ${mat7} do aluno.`));
 if (presFil > 10 || presFil < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat7} é ${presFil}`);
+  console.log(`A presença de ${mat7} é ${presFil}`);
 }
 
 //Sociologia
 
 let notaSocio = parseFloat(prompt(`Insira a nota ${mat8} do aluno.`));
 if (notaSocio > 10 || notaSocio < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat8} é ${notaSocio}`);
+  console.log(`A nota de ${mat8} é ${notaSocio}`);
 }
-console.log(`A nota de ${mat8} é ${notaSocio}`);
 
 let presSocio = parseInt(prompt(`Insira a presença de ${mat8} do aluno.`));
 if (presSocio > 10 || presSocio < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat8} é ${presSocio}`);
+  console.log(`A presença de ${mat8} é ${presSocio}`);
 }
 
 //Literatura
 
 let notaLit = parseFloat(prompt(`Insira a nota ${mat9} do aluno.`));
 if (notaLit > 10 || notaLit < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat9} é ${notaLit}`);
+  console.log(`A nota de ${mat9} é ${notaLit}`);
 }
-console.log(`A nota de ${mat9} é ${notaLit}`);
 
 let presLit = parseInt(prompt(`Insira a presença de ${mat9} do aluno.`));
 if (presLit > 10 || presLit < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat9} é ${presLit}`);
+  console.log(`A presença de ${mat9} é ${presLit}`);
 }
 
 //Educação Física
 
 let notaEdf = parseFloat(prompt(`Insira a nota ${mat10} do aluno.`));
 if (notaEdf > 10 || notaEdf < 0) {
-  console.log(`A nota é inválida!`);
+  alert(`A nota é inválida!`);
 } else {
-  alert(`A nota de ${mat10} é ${notaEdf}`);
+  console.log(`A nota de ${mat10} é ${notaEdf}`);
 }
-console.log(`A nota de ${mat10} é ${notaEdf}`);
-
 let presEdf = parseInt(prompt(`Insira a presença de ${mat10} do aluno.`));
 if (presEdf > 10 || presEdf < 0) {
-  console.log(`A presença é inválida!`);
+  alert(`A presença é inválida!`);
 } else {
-  alert(`A presença de ${mat10} é ${presEdf}`);
+  console.log(`A presença de ${mat10} é ${presEdf}`);
 }
 
 let somaNota =
